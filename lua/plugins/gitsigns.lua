fbb5060e460086bdffc50d43be6a0213f3d08ff0
Benjamin Hottell <benjaminhottell@gmail.com>
2026-03-14 12:56:34 -0400

    gitsigns chunk navigation

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -9,5 +9,33 @@ return {
 		},
 		opts = {
 		},
+
+		keys = {
+			{
+				"]c",
+				function()
+					if vim.wo.diff then
+						vim.cmd.normal({"]c", bang = true})
+					else
+						local gitsigns = require("gitsigns")
+						gitsigns.nav_hunk("next")
+					end
+				end,
+				desc = "(gitsigns) navigate to next chunk",
+			},
+			{
+				"[c",
+				function()
+					if vim.wo.diff then
+						vim.cmd.normal({"[c", bang = true})
+					else
+						local gitsigns = require("gitsigns")
+						gitsigns.nav_hunk("prev")
+					end
+				end,
+				desc = "(gitsigns) navigate to next chunk",
+			},
+		},
+
 	},
 }
