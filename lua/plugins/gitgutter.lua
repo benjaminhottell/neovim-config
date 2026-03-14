1652970543cd7f30d4205f4216221752c954ccf7
Benjamin Hottell <benjaminhottell@gmail.com>
2026-03-14 12:41:41 -0400

    Add gitgutter config for reference

---
+++ lua/plugins/gitgutter.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"airblade/vim-gitgutter",
+		enabled = false,
+		event = {
+			"VeryLazy",
+		},
+		cmd = {
+			"GitGutterDisable",
+			"GitGutterEnable",
+			"GitGutterToggle",
+			"GitGutterBufferDisable",
+			"GitGutterBufferEnable",
+			"GitGutterBufferToggle",
+			"GitGutterSignsEnable",
+			"GitGUtterSignsDisable",
+			"GitGutterSignsToggle",
+			"GitGutterLineHighlightsEnable",
+			"GitGutterLineHighlightsDisable",
+			"GitGutterLineHighlightsToggle",
+			"GitGutterLineNrHighlightsEnable",
+			"GitGutterLineNrHighlightsDisable",
+			"GitGutterLineNrHighlightsToggle",
+		},
+	}
+}
