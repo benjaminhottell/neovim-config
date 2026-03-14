47ecd819cdfab9bd15702b87437cd434feb12a58
Benjamin Hottell <benjaminhottell@gmail.com>
2026-03-14 13:06:35 -0400

    Quit nvim if only snacks explorer is open

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -70,12 +70,38 @@ return {
 	},
 
 	init = function()
+
 		vim.api.nvim_set_hl(0, "SnacksIndentScope", {
 			link = "GruvBoxBg4",
 		})
 		vim.api.nvim_set_hl(0, "SnacksIndent", {
 			link = "GruvBoxBg1",
 		})
+
+		-- Quit nvim when explorer is last open window
+		-- https://github.com/folke/snacks.nvim/discussions/1346
+		vim.api.nvim_create_autocmd('QuitPre', {
+			callback = function()
+				local snacks_windows = {}
+				local floating_windows = {}
+				local windows = vim.api.nvim_list_wins()
+				for _, w in ipairs(windows) do
+					local filetype = vim.api.nvim_get_option_value('filetype', { buf = vim.api.nvim_win_get_buf(w) })
+					if filetype:match('snacks_') ~= nil then
+						table.insert(snacks_windows, w)
+					elseif vim.api.nvim_win_get_config(w).relative ~= '' then
+						table.insert(floating_windows, w)
+					end
+				end
+				if 1 == #windows - #floating_windows - #snacks_windows then
+					-- Should quit, so we close all Snacks windows.
+					for _, w in ipairs(snacks_windows) do
+						vim.api.nvim_win_close(w, true)
+					end
+				end
+			end,
+		})
+
 	end
 
 }
